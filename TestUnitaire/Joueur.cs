669675d354c3750198a1dc6711e80cb73d322d2b
Charif <91829231+CharifMah@users.noreply.github.com>
2022-05-21 19:00:00 +0200

    Update TestUnitVie

--- TestUnitaire/Joueur.cs
+++ TestUnitaire/Joueur.cs
@@ -16,10 +16,24 @@ namespace TestUnitaire
             {
                 Vie v = new Vie(new Canvas(), 10, 50);
                 Assert.Equal(v._Vie, 10);
+
                 v.AddLife(50);
                 Assert.Equal(v._Vie, 50);
+
                 v.RemoveLife(49);
                 Assert.Equal(v._Vie, 1);
+
+                v.AddLife(19);
+                Assert.Equal(v._Vie, 20);
+
+                v.RemoveEmptyLife(5);
+                Assert.Equal(v._Vie, 15);
+
+                v.AddEmptyLife(5);
+                Assert.Equal(v._Vie, 15);
+
+                v.AddLife(100);
+                Assert.Equal(v._Vie, 55);
             });
             t.SetApartmentState(ApartmentState.STA);
             t.Start();         
