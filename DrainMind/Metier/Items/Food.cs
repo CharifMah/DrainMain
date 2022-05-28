cdb674d59e4902402985bdcac133ee00c89e3d49
Ness <inesecharoux@gmail.com>
2022-05-29 00:37:05 +0200

    Food.cs : correction commentaire

--- DrainMind/Metier/Items/Food.cs
+++ DrainMind/Metier/Items/Food.cs
@@ -12,20 +12,34 @@ using System.Windows.Media;
 
 namespace DrainMind.Metier.Items
 {
+    /// <summary>
+    /// Food, allows to gain life back
+    /// </summary>
     public class Food : IUTGame.GameItem
     {
         //ennemies's number
         private static int foodnumber = 0;
 
-
+        /// <summary>
+        /// Create the food
+        /// </summary>
+        /// <param name="x">axis x</param>
+        /// <param name="y">axis y</param>
         public Food(double x, double y) : base(x, y, DrainMindView.MainCanvas, DrainMindGame.Instance, "FoodPeach.png")
         {
             ++foodnumber;
             this.ChangeScale(2,2);
         }
 
+        /// <summary>
+        /// Name of the food
+        /// </summary>
         public override string TypeName => "Peach";
 
+        /// <summary>
+        /// Executes the effect of the collision
+        /// </summary>
+        /// <param name="other">other object</param>
         public override void CollideEffect(GameItem other)
         {
             if (other.TypeName == "Joueur")
