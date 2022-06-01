076f5701b55965eb7a1c98555eb3062824447600
Charif <91829231+CharifMah@users.noreply.github.com>
2022-06-01 15:43:08 +0200

    Change boss stats

--- DrainMind/metier/enemie/EnemieBoss.cs
+++ DrainMind/metier/enemie/EnemieBoss.cs
@@ -18,11 +18,11 @@ namespace DrainMind.metier.enemie
         /// <param name="spritename">name of sprite</param>
         public EnemieBoss(double x, double y, string spritename = "Enemie/boss.png") : base(x, y, spritename)
         {
-            this._speed = 5;
+            this._speed = 7;
             this._XPpoint = 100;
-            this._damage = 3;
-            this._life = 10;
-            this._maxlife = 10;
+            this._damage = 1;
+            this._life = 7;
+            this._maxlife = 7;
             this._soundHit = "Hit5.mp3";
             this._traverseEnemie = true;
             this._minspeed = _speed;
